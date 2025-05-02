f05eea2586895a20985d07ae9064be397522912d
João Cabral <cabral047dev@gmail.com>
2025-05-01 21:50:59 -0300

    feat: implement GetUrlByShortCodeRepositoryService
    Add GetUrlByShortCodeRepositoryService to retrieve URLs by short code.
    
    Implement interface for getting URL by short code.

---
+++ src/infrastructure/database/repositories/url/get-url-by-shortcode-repository.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { UrlEntity } from 'src/core/domain/entities/url.entity';
+import { UrlMapper } from '../../mappers/url.mapper';
+import { EntityNotFoundError, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UrlModel } from '../../models/url.model';
+import { DatabaseErrorHandler } from '../../utils/db-error-handler';
+import { GetUrlByShortCodeRepositoryInterface } from 'src/core/domain/repositories/get-url-by-shortcode-repository.interface';
+
+@Injectable()
+export class GetUrlByShortCodeRepositoryService implements GetUrlByShortCodeRepositoryInterface {
+  constructor(
+    @InjectRepository(UrlModel)
+    private readonly urlRepository: Repository<UrlModel>,
+    private readonly errorHandler: DatabaseErrorHandler,
+  ) {}
+  async findByShortCode(shortCode: string): Promise<UrlEntity> {
+    try {
+      const url = await this.urlRepository.findOne({ where: { shortCode } });
+      if (!url) {
+        throw new EntityNotFoundError('URL', shortCode);
+      }
+      return UrlMapper.toDomain(url);
+    } catch (error) {
+      throw this.errorHandler.handleError(error, 'URL');
+    }
+  }
+}
