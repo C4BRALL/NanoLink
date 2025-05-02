fbc562deb2c25a213a758c9b591f8975c6d34c15
João Cabral <cabral047dev@gmail.com>
2025-05-02 15:17:35 -0300

    feat: ensure GetUrlByShortCodeService to calls updateUrlClickCountRepositoryService

--- src/core/use-cases/url/get-url-by-shortcode.service.ts
+++ src/core/use-cases/url/get-url-by-shortcode.service.ts
@@ -2,20 +2,25 @@ import { Inject, Injectable } from '@nestjs/common';
 import { GetUrlByShortCodeRepositoryInterface } from 'src/core/domain/repositories/get-url-by-shortcode-repository.interface';
 import { GetUrlByShortCodeInterface } from 'src/core/domain/use-cases/get-url-by-shortcode.interface';
 import { UrlRetrievalFailedError } from '../errors/url-error';
+import { UrlEntity } from 'src/core/domain/entities/url.entity';
+import { UpdateUrlClickCountRepositoryInterface } from 'src/core/domain/repositories/update-url-clickcount-repository.interface';
 
 @Injectable()
 export class GetUrlByShortCodeService implements GetUrlByShortCodeInterface {
   constructor(
     @Inject('GetUrlByShortCodeRepositoryInterface')
     private readonly urlRepository: GetUrlByShortCodeRepositoryInterface,
+    @Inject('UpdateUrlClickCountRepositoryInterface')
+    private readonly updateUrlRepository: UpdateUrlClickCountRepositoryInterface,
   ) {}
-  async execute(params: GetUrlByShortCodeInterface.Params): Promise<GetUrlByShortCodeInterface.Output> {
+  async execute(params: GetUrlByShortCodeInterface.Params): Promise<UrlEntity> {
     try {
       const url = await this.urlRepository.findByShortCode(params.shortCode);
-      if (!url?.id || url.isDeleted() === true) {
-        return { url: null };
-      }
-      return { url };
+
+      url.incrementClickCount();
+
+      await this.updateUrlRepository.update(url);
+      return url;
     } catch (error) {
       throw new UrlRetrievalFailedError(params.shortCode, error);
     }
