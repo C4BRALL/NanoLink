3d1314b30703bae68c0706572c406ebea3a452c3
João Cabral <cabral047dev@gmail.com>
2025-05-03 01:48:48 -0300

    feat: ensure CreateUserInterface

---
+++ src/core/domain/use-cases/user/create-user.interface.ts
@@ -0,0 +1,18 @@
+import { UserEntity } from '../../entities/user.entity';
+
+export interface CreateUserInterface {
+  execute(user: CreateUserInterface.Params): Promise<CreateUserInterface.Output>;
+}
+
+export namespace CreateUserInterface {
+  export type Params = {
+    name: string;
+    email: string;
+    password: string;
+  };
+
+  export type Output = {
+    user: UserEntity;
+    token: string;
+  };
+}
