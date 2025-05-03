e3b2c86ac87d63de0d89f23aba486a5d1ace36d8
João Cabral <cabral047dev@gmail.com>
2025-05-03 17:31:44 -0300

    feat: Ensure GenerateTokenService

--- src/core/use-cases/use-cases.module.ts
+++ src/core/use-cases/use-cases.module.ts
@@ -3,10 +3,13 @@ import { CreateUrlService } from './url/create-url.service';
 import { InfrastructureModule } from 'src/infrastructure/infrastructure.module';
 import { ConfigurationModule } from 'src/infrastructure/config/config.module';
 import { GetUrlByShortCodeService } from './url/get-url-by-shortcode.service';
+import { CreateUserService } from './user/create-user.service';
+import { GenerateTokenService } from './auth/generate-token.service';
+import { AuthModule } from 'src/infrastructure/auth/auth.module';
 
 @Module({
-  imports: [InfrastructureModule, ConfigurationModule],
-  providers: [CreateUrlService, GetUrlByShortCodeService],
-  exports: [CreateUrlService, GetUrlByShortCodeService],
+  imports: [InfrastructureModule, ConfigurationModule, AuthModule],
+  providers: [CreateUrlService, GetUrlByShortCodeService, CreateUserService, GenerateTokenService],
+  exports: [CreateUrlService, GetUrlByShortCodeService, CreateUserService, GenerateTokenService],
 })
 export class UseCasesModule {}
